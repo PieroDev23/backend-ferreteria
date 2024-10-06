e341a3f285bdf40a1d1202855ceb062deccc1f3a
PieroDev23 <pierodavilaaguirre22@gmail.com>
2024-10-05 21:08:34 -0500

    get all products

--- src/products/service.ts
+++ src/products/service.ts
@@ -1,1 +1,29 @@
-export class ProductsService {}
+import { ProductsRepository } from "./repository";
+
+export class ProductsService {
+  static async getProducts() {
+    const rawProducts = await ProductsRepository.getAllProducts();
+
+    return rawProducts.map(({ products, inventory, discounts, categories }) => {
+      const { id, name, description, sku, price, image, createdAt, updatedAt } =
+        products;
+      const { quantity } = inventory;
+      const discount = discounts ? discounts.percentage : 0;
+      const totalPrice = price - price * (discount / 100);
+      const { name: categorieName } = categories;
+
+      return {
+        id,
+        name,
+        categorie: categorieName,
+        description,
+        sku,
+        image,
+        createdAt,
+        updatedAt,
+        quantity,
+        totalPrice,
+      };
+    });
+  }
+}
